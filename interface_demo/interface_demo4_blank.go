6d1b35d0ec184d0adb74a23046f188f16f642020
morningcat <morningcat2018@qq.com>
2021-05-06 04:39:42 +0800

    interface

--- interface_demo/interface_demo4_blank.go
+++ interface_demo/interface_demo4_blank.go
@@ -6,10 +6,12 @@ import "fmt"
 type blank interface{} // 所有类型都实现了空接口 即所有类型都相当于这种接口类型的变量
 
 // 空接口 作为入参
+// interface{} 空接口类型
 func BlankInterfaceDemo1(i interface{}) {
 	fmt.Println(i)
 }
 
+// 空接口列表
 func BlankInterfaceDemo2(is ...interface{}) {
 	for i, v := range is {
 		fmt.Println(i, v)
