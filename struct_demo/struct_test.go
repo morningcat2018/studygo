09d32b565607159ca36eebc596e97c38d5872556
morningcat <morningcat2018@qq.com>
2021-10-12 02:27:24 +0800

    1

--- struct_demo/struct_test.go
+++ struct_demo/struct_test.go
@@ -2,7 +2,6 @@ package struct_demo
 
 import (
 	"fmt"
-	"testing"
 	"time"
 )
 
@@ -31,47 +30,3 @@ func (p Person) printPerson() {
 }
 
 ////
-
-func NewPerson(idNo, name string) *Person {
-	return &Person{idNo: idNo, name: name}
-}
-
-// 作用于特定类型的函数
-func (p Person) GetPersonStr() string { //  值接收者
-	return fmt.Sprintf(p.idNo, p.name, p.profession, p.age, (&p.birthday).Format(YYYY_MM_DD))
-}
-
-func TestStruct6(t *testing.T) {
-	var p *Person = NewPerson("3419002", "morningcat")
-	fmt.Println(p.GetPersonStr())
-}
-
-func (p *Person) ChangrPersonName(name string) { // 指针接收者
-	p.name = name
-}
-
-func StructDemo7() {
-	var p = NewPerson("3419002", "morningcat")
-	fmt.Println(p.name)
-	p.ChangrPersonName("world")
-	fmt.Println(p.name)
-}
-
-// 任意自定义类型都可以加方法
-
-func (x myInt) helloInt() {
-	fmt.Printf("Hello %d\n", x)
-}
-
-func StructDemo8() {
-	var x myInt = 12
-	x.helloInt()
-}
-
-// 只能给自己 package 中的`自定义类型`添加方法
-
-func StructDemo10() {
-	var x int32 = 12
-	var y = int32(12) // 另一种定义方法
-	fmt.Println(x, y)
-}
